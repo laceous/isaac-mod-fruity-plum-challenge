89aca9d3532104d2cc67188dc9694fe29eb0ceab
laceous <laceous@gmail.com>
2023-05-18 23:13:06 -0700

    finish by level 20

--- fruity plum challenge/main.lua
+++ fruity plum challenge/main.lua
@@ -1,7 +1,8 @@
 local mod = RegisterMod('Fruity Plum Challenge', 1)
 local game = Game()
 
-mod.bossId = 84 -- baby plum
+mod.babyPlumBossId = 84
+mod.rngShiftIdx = 35
 mod.playerHash = nil
 
 function mod:onGameStart(isContinue)
@@ -37,6 +38,48 @@ end
 
 function mod:onGameExit()
   mod.playerHash = nil
+end
+
+function mod:onNewLevel()
+  if not mod:isChallenge() then
+    return
+  end
+  
+  -- force baby plum by XX
+  -- check past this number just in case something like book of rev was used
+  -- if we need to go deeper then add some code to parse this into an integer
+  local basementNum = mod:getBasementNum()
+  
+  if basementNum == 'XX' or
+     basementNum == 'XXI' or
+     basementNum == 'XXII' or
+     basementNum == 'XXIII' or
+     basementNum == 'XXIV' or
+     basementNum == 'XXV'
+  then
+    local seeds = game:GetSeeds()
+    local level = game:GetLevel()
+    local stage = level:GetStage()
+    local rooms = level:GetRooms()
+    local bossRoomIdx = rooms:Get(level:GetLastBossRoomListIndex()).SafeGridIndex
+    local bossRoom = level:GetRoomByIdx(bossRoomIdx, -1) -- read/write
+    
+    -- check for 1x1, but it should always be the case in the basement
+    if bossRoom and bossRoom.Data and bossRoom.Data.Subtype ~= mod.babyPlumBossId and bossRoom.Data.Shape == RoomShape.ROOMSHAPE_1x1 then
+      local roomIdx = level:GetCurrentRoomIndex()
+      
+      local rng = RNG()
+      rng:SetSeed(seeds:GetStageSeed(stage), mod.rngShiftIdx)
+      local babyPlumRooms = { '5160', '5161', '5162', '5163', '5165' } -- 5164 only has 3 doors
+      local babyPlumRoom = babyPlumRooms[rng:RandomInt(#babyPlumRooms) + 1]
+      
+      Isaac.ExecuteCommand('goto s.boss.' .. babyPlumRoom)
+      local dbg = level:GetRoomByIdx(GridRooms.ROOM_DEBUG_IDX, -1)
+      bossRoom.Data = dbg.Data
+      
+      game:StartRoomTransition(roomIdx, Direction.NO_DIRECTION, RoomTransitionAnim.FADE, nil, -1)
+    end
+  end
 end
 
 function mod:onUpdate()
@@ -98,10 +141,41 @@ function mod:getCard(rng, card, includeCards, includeRunes, onlyRunes)
   end
 end
 
+function mod:getBasementNum()
+  local level = game:GetLevel()
+  local name = level:GetName()
+  local num = ''
+  
+  -- Basement I, Basement III?
+  -- loop backwards until we find a space
+  -- there might be punctuation at the very end
+  -- this works for multi-language
+  for i = string.len(name), 1, -1 do
+    local c = string.sub(name, i, i)
+    if c == ' ' then
+      break
+    elseif mod:isRomanNum(c) then
+      num = c .. num
+    end
+  end
+  
+  return num
+end
+
+function mod:isRomanNum(c)
+  for _, v in ipairs({ 'I', 'V', 'X', 'L', 'C', 'D', 'M' }) do
+    if v == c then
+      return true
+    end
+  end
+  
+  return false
+end
+
 function mod:doBabyPlumRoomLogic()
   local room = game:GetRoom()
   
-  if room:IsCurrentRoomLastBoss() and room:GetBossID() == mod.bossId and room:IsClear() then
+  if room:IsCurrentRoomLastBoss() and room:GetBossID() == mod.babyPlumBossId and room:IsClear() then
     mod:removeTrapdoors()
     mod:spawnTrophy(room:GetCenterPos())
   end
@@ -142,6 +216,7 @@ end
 
 mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.onGameStart)
 mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, mod.onGameExit)
+mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.onNewLevel)
 mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.onUpdate)
 mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, mod.onPlayerInit, 0) -- 0 is player, 1 is co-op baby
 mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.onPeffectUpdate, PlayerType.PLAYER_ISAAC)
