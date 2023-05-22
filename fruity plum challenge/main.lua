44b999fefec423974982f353d2ad35f2a79caabe
laceous <laceous@gmail.com>
2023-05-21 19:28:38 -0700

    add giant challenge

--- fruity plum challenge/main.lua
+++ fruity plum challenge/main.lua
@@ -6,7 +6,7 @@ mod.rngShiftIdx = 35
 mod.playerHash = nil
 
 function mod:onGameStart(isContinue)
-  if not mod:isChallenge() then
+  if not mod:isChallenge() and not mod:isGiantChallenge() then
     return
   end
   
@@ -22,7 +22,6 @@ function mod:onGameStart(isContinue)
     itemPool:RemoveCollectible(CollectibleType.COLLECTIBLE_D100)
     itemPool:RemoveCollectible(CollectibleType.COLLECTIBLE_D_INFINITY)
     itemPool:RemoveCollectible(CollectibleType.COLLECTIBLE_ESAU_JR)
-    itemPool:RemoveCollectible(CollectibleType.COLLECTIBLE_FRUITY_PLUM)
     itemPool:RemoveCollectible(CollectibleType.COLLECTIBLE_GENESIS)
     itemPool:RemoveCollectible(CollectibleType.COLLECTIBLE_LEMEGETON)
     itemPool:RemoveCollectible(CollectibleType.COLLECTIBLE_METRONOME)
@@ -32,6 +31,12 @@ function mod:onGameStart(isContinue)
     itemPool:RemoveTrinket(TrinketType.TRINKET_ERROR)
     itemPool:RemoveTrinket(TrinketType.TRINKET_EXPANSION_PACK)
     itemPool:RemoveTrinket(TrinketType.TRINKET_M)
+    
+    if mod:isChallenge() then
+      itemPool:RemoveCollectible(CollectibleType.COLLECTIBLE_FRUITY_PLUM)
+    elseif mod:isGiantChallenge() then
+      itemPool:RemoveCollectible(CollectibleType.COLLECTIBLE_PLUM_FLUTE)
+    end
   end
 end
 
@@ -39,8 +44,17 @@ function mod:onGameExit()
   mod.playerHash = nil
 end
 
+function mod:onNewRoom()
+  if not mod:isGiantChallenge() then
+    return
+  end
+  
+  local player = game:GetPlayer(0)
+  player:UseActiveItem(CollectibleType.COLLECTIBLE_PLUM_FLUTE, false, false, true, false, -1, 0)
+end
+
 function mod:onNewLevel()
-  if not mod:isChallenge() then
+  if not mod:isChallenge() and not mod:isGiantChallenge() then
     return
   end
   
@@ -51,8 +65,8 @@ function mod:onNewLevel()
     local level = game:GetLevel()
     local stage = level:GetStage()
     local rooms = level:GetRooms()
-    local bossRoomIdx = rooms:Get(level:GetLastBossRoomListIndex()).SafeGridIndex
-    local bossRoom = level:GetRoomByIdx(bossRoomIdx, -1) -- read/write
+    local bossRoomRo = rooms:Get(level:GetLastBossRoomListIndex()) -- read-only
+    local bossRoom = bossRoomRo and level:GetRoomByIdx(bossRoomRo.SafeGridIndex, -1) -- read/write
     
     -- check for 1x1, but it should always be the case in the basement
     if bossRoom and bossRoom.Data and bossRoom.Data.Subtype ~= mod.babyPlumBossId and bossRoom.Data.Shape == RoomShape.ROOMSHAPE_1x1 then
@@ -73,11 +87,11 @@ function mod:onNewLevel()
 end
 
 function mod:onUpdate()
-  if not mod:isChallenge() then
+  if not mod:isChallenge() and not mod:isGiantChallenge() then
     return
   end
   
-  if mod:countFruityPlums() == 0 then
+  if mod:isChallenge() and mod:countFruityPlums() == 0 then
     mod:killAllPlayers()
   end
   
@@ -86,7 +100,7 @@ end
 
 -- filtered to 0-Player
 function mod:onPlayerInit(player)
-  if not mod:isChallenge() then
+  if not mod:isChallenge() and not mod:isGiantChallenge() then
     return
   end
   
@@ -94,21 +108,24 @@ function mod:onPlayerInit(player)
     return
   end
   
-  if mod:countFruityPlums() == 0 then
-    -- limit to one fruity plum
-    player:AddCollectible(CollectibleType.COLLECTIBLE_FRUITY_PLUM, 0, true, ActiveSlot.SLOT_PRIMARY, 0)
+  if mod:isChallenge() then
+    if mod:countFruityPlums() == 0 then
+      -- limit to one fruity plum
+      player:AddCollectible(CollectibleType.COLLECTIBLE_FRUITY_PLUM, 0, true, ActiveSlot.SLOT_PRIMARY, 0)
+    end
+    for _, v in ipairs({ TrinketType.TRINKET_BABY_BENDER, TrinketType.TRINKET_FORGOTTEN_LULLABY }) do
+      player:AddTrinket(v)
+      player:UseActiveItem(CollectibleType.COLLECTIBLE_SMELTER, false, false, true, false, -1, 0)
+    end
   end
-  for _, v in ipairs({ TrinketType.TRINKET_BABY_BENDER, TrinketType.TRINKET_FORGOTTEN_LULLABY }) do
-    player:AddTrinket(v)
-    player:UseActiveItem(CollectibleType.COLLECTIBLE_SMELTER, false, false, true, false, -1)
-  end
+  
   player:AddTrinket(TrinketType.TRINKET_AAA_BATTERY)
   mod.playerHash = GetPtrHash(player)
 end
 
 -- filtered to PLAYER_ISAAC
 function mod:onPeffectUpdate(player)
-  if not mod:isChallenge() then
+  if not mod:isChallenge() and not mod:isGiantChallenge() then
     return
   end
   
@@ -120,8 +137,19 @@ function mod:onPeffectUpdate(player)
   end
 end
 
+function mod:onFamiliarUpdate(familiar)
+  if not mod:isGiantChallenge() then
+    return
+  end
+  
+  -- 3 to 7, at 7 she leaves, reset to 3
+  if familiar.State == 7 then
+    familiar.State = 3
+  end
+end
+
 function mod:getCard(rng, card, includeCards, includeRunes, onlyRunes)
-  if not mod:isChallenge() then
+  if not mod:isChallenge() and not mod:isGiantChallenge() then
     return
   end
   
@@ -137,6 +165,7 @@ function mod:getBasementNum()
   
   -- Basement I, Basement III?
   -- there might be punctuation at the very end
+  -- XL is disabled via cursefilter so we don't need to worry about: Basement XL! XL
   -- this works for multi-language
   local num = string.match(name, ' ([IVXLCDM]+)%p*$')
   
@@ -148,11 +177,13 @@ function mod:getBasementNum()
 end
 
 -- this doesn't validate roman numerals
+-- IIII will return 4
+-- IL will return 51 rather than 49
 function mod:romanToInt(s)
   local num = 0
   
-  local i = 1
-  while i <= string.len(s) do
+  local i, l = 1, string.len(s)
+  while i <= l do
     local c = string.sub(s, i, i + 1)
     local n = string.len(c) == 2 and mod:getRomanVal(c)
     if n then
@@ -234,10 +265,17 @@ function mod:isChallenge()
   return challenge == Isaac.GetChallengeIdByName('Fruity Plum Challenge')
 end
 
+function mod:isGiantChallenge()
+  local challenge = Isaac.GetChallenge()
+  return challenge == Isaac.GetChallengeIdByName('Fruity Plum Challenge (Giant)')
+end
+
 mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.onGameStart)
 mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, mod.onGameExit)
 mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.onNewLevel)
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.onNewRoom)
 mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.onUpdate)
 mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, mod.onPlayerInit, 0) -- 0 is player, 1 is co-op baby
 mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.onPeffectUpdate, PlayerType.PLAYER_ISAAC)
+mod:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, mod.onFamiliarUpdate, FamiliarVariant.BABY_PLUM)
 mod:AddCallback(ModCallbacks.MC_GET_CARD, mod.getCard)
